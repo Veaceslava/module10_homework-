7fc4f4282ea18fb8273d8b4d4e213bf43b6677ff
Slava <118695555+Veaceslava@users.noreply.github.com>
2022-12-19 01:16:37 +0200

    Add files via upload

---
+++ Md10№2/script.js
@@ -0,0 +1,17 @@
+"use stirct";
+
+/* Модудь 10.1*/
+
+
+/* Модудь 10.1 / Задание 2 */
+
+let x = 2;
+let type = typeof x
+switch (type) {
+    case "number":
+    console.log("x - число");
+    break; 
+}
+
+
+
