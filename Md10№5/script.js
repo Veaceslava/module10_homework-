da2a83af2d3d04acbbc1c37a34d7a3e69f96ba9c
Slava <118695555+Veaceslava@users.noreply.github.com>
2022-12-19 01:17:27 +0200

    Add files via upload

---
+++ Md10№5/script.js
@@ -0,0 +1,19 @@
+"use stirct";
+
+/* Модудь 10.1*/
+
+
+/* Модудь 10.1 / Задание 5 */
+
+
+
+let arr = [1,2,3];
+let result = arr.map(function(item, index, array) {
+
+  return item + 1
+
+});
+console.log(arr);
+[1, 2, 3]
+console.log(result);
+[2, 3, 4]
