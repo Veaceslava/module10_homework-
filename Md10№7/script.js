282abd0b6447fa8139f58b43531e57aaacece2fd
Slava <118695555+Veaceslava@users.noreply.github.com>
2022-12-19 01:18:00 +0200

    Add files via upload

---
+++ Md10№7/script.js
@@ -0,0 +1,17 @@
+"use stirct";
+
+/* Модудь 10.1*/
+
+
+/* Модудь 10.1 / Задание 7 */
+
+
+let arr = [1, 2, 3, 4, 5];
+console.log( arr.filter(elem => elem % 2 == 0) );
+
+
+for (let i = 1; i < 10; i += 1) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
